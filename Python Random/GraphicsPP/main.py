6a29c574a29219a1faf8d6c7b089db7994cf5cae
skrefi <srazvan43@yahoo.com>
2020-03-05 23:28:37 +0200

    yes

--- Python Random/GraphicsPP/main.py
+++
@@ -1,6 +0,0 @@
-from GraphicsPP import *
-
-
-s = GraphWin()
-b = Box(s, Point(0, 0), Point(50, 50), "red", "Hello World")
-s.getMouse()
--- Python Random/Temp/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-
-# def getRandomList(length, min, max):
-#     l = []
-#     base = r(2,)
-#     for _ in range(length):
