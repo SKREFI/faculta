5ce3a223a50592b8389b2029896f1c22d7977ad6
skrefi <srazvan43@yahoo.com>
2020-03-17 00:33:38 +0200

    ...

--- projects/exercise/run.py
+++
@@ -1,9 +0,0 @@
-# from pathlib import
-from os import getcwd as pwd
-from os import path
-
-from ImportEngine.Importer import Import
-
-csv_path = pwd() + '/data/cats.csv'
-
-print(Import.parse(csv_path))
